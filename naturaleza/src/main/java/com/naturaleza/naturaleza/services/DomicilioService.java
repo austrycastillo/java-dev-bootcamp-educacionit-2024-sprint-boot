eaff1bc0fef2485d9c9d6cf5353d291c730d0611
Austry Castillo <castilloaustry@gmail.com>
2024-09-09 21:35:04 -0300

    Add files via upload
    clase 48: Crud con jpa repository

---
+++ naturaleza/src/main/java/com/naturaleza/naturaleza/services/DomicilioService.java
@@ -0,0 +1,41 @@
+package com.naturaleza.naturaleza.services;
+
+import com.naturaleza.naturaleza.entities.Domicilio;
+import com.naturaleza.naturaleza.exceptions.DomicilioNoEncontradoException;
+import com.naturaleza.naturaleza.repositories.DomicilioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DomicilioService {
+    @Autowired
+    private DomicilioRepository domicilioRepository;
+    //CREAR NUESTROS METODOS UTILIZANDO EL REPOSITORIO Y SUS METODOS JPA PARA HACER EL CRUD
+    //insert
+    public Domicilio crearDomicilio(Domicilio domicilio) {
+        return domicilioRepository.save(domicilio);
+    }
+    //select
+    public List<Domicilio> listarDomicilios() {
+        return  domicilioRepository.findAll();
+    }
+    public Domicilio buscarDomicilioPorId(Long id) {
+        return  domicilioRepository.findById(id).orElseThrow(
+                ()->new DomicilioNoEncontradoException("Domicilio no encontrado, id " + id)
+        );
+    }
+    //delete
+    public void eliminarDomicilio(Long id) {
+        domicilioRepository.deleteById(id);
+    }
+    //update
+    public Domicilio actualizarDomicilio(Long id, Domicilio domicilioNuevo){
+        Domicilio domicilioActual = buscarDomicilioPorId(id);
+        domicilioActual.setCodigo(domicilioNuevo.getCodigo());
+        domicilioActual.setDireccion(domicilioNuevo.getDireccion());
+        return domicilioRepository.save(domicilioActual);
+    }
+
+}
