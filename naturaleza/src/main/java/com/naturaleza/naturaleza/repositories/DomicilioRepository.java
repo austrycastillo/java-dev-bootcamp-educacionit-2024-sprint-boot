1649bb69f8875c75fd76f14f2579b80d2f4005b3
Austry Castillo <castilloaustry@gmail.com>
2024-09-09 21:33:54 -0300

    Delete naturaleza directory

--- naturaleza/src/main/java/com/naturaleza/naturaleza/repositories/DomicilioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.naturaleza.naturaleza.repositories;
-
-import com.naturaleza.naturaleza.entities.Domicilio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
-}
--- naturaleza/src/main/java/com/naturaleza/naturaleza/repositories/EquipajeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.naturaleza.naturaleza.repositories;
-
-import com.naturaleza.naturaleza.entities.Equipaje;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipajeRepository extends JpaRepository<Equipaje, Long> {
-}
