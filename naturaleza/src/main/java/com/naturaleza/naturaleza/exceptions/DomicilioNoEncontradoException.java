8abd685182c06bb4434c502880254b23f32a2934
Austry Castillo <castilloaustry@gmail.com>
2024-09-23 21:33:07 -0300

    Delete naturaleza directory

--- naturaleza/src/main/java/com/naturaleza/naturaleza/exceptions/DomicilioNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.naturaleza.naturaleza.exceptions;
-
-public class DomicilioNoEncontradoException extends RuntimeException {
-    public DomicilioNoEncontradoException(String mensaje){
-        super(mensaje);
-    }
-}
--- naturaleza/src/main/java/com/naturaleza/naturaleza/repositories/DomicilioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.naturaleza.naturaleza.repositories;
-
-import com.naturaleza.naturaleza.entities.Domicilio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
-}
